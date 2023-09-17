f326853c7b5a3411180f9222bcb6e4bfd409450a
Lotx-001 <72420846+Lotx-001@users.noreply.github.com>
2023-09-18 02:10:07 +0300

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -155,6 +155,8 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.K7:
       ret.mass = 1730.
       ret.wheelbase = 2.85
+      ret.steerRatio = 15.4            
+      tire_stiffness_factor = 0.7
     elif candidate == CAR.KIA_SPORTAGE_5TH_GEN:
       ret.mass = 1700. + STD_CARGO_KG  # weight from SX and above trims, average of FWD and AWD versions
       ret.wheelbase = 2.756
